744e646c8524bb58bab7323c616dd4c62d6e51e6
Blackstar84 <carlosalmada84@gmail.com>
2023-02-22 21:22:40 -0300

    se agregan todos los archivos

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
 const app = express();
+const routes = require('./routes/routes');
 
 
 
-app.get('/', (req, res) => {
-      
-  
-});
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+
+//view engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', hbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//routes
+app.use(routes);
+
+
+
+//server
+app.listen(3000, () => console.log('Listening in port 3000'));
